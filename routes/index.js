c788eaa9c51015168926f5c6e836f41e643ac9ac
StepkaP <Steve6305@gmail.com>
2021-11-23 23:53:07 +0700

    Extract calc code and contract metadata

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const Web3 = require('web3');
 const bodyParser = require("body-parser");
 
-const infuraKey = process.env.INFURA_KEY || null;
-const rinkebyWallet = process.env.RINKEBY_WALLET_ADDRESS || null;
+const calc = require("../src/calc");
 
 router.use(bodyParser.urlencoded({extended:true}));
 router.use(bodyParser.json());
-router.post('/calculate', calculate);
-
-async function calculate(req, res) {
-  const first = parseInt(req.body.firstOperand);
-  const second =  parseInt(req.body.secondOperand);
-  const operator =  parseInt(req.body.operator);
-
-  try {
-    const result = await getBinaryOperation(first, second, operator);
-    res.send(result.toString());
-  } catch (e) {
-    res.send(e.message)
-  }
-}
-
-async function getBinaryOperation(firstOperand, secondOperand, operator) {
-
-  const web3 = new Web3(new Web3.providers.HttpProvider( `https://rinkeby.infura.io/v3/${infuraKey}`));
-  const calcContract = new web3.eth.Contract([
-    {
-      "inputs": [
-        {
-          "internalType": "int256",
-          "name": "a",
-          "type": "int256"
-        },
-        {
-          "internalType": "int256",
-          "name": "b",
-          "type": "int256"
-        },
-        {
-          "internalType": "enum Calc.BinaryOperator",
-          "name": "op",
-          "type": "uint8"
-        }
-      ],
-      "name": "getBinaryOperation",
-      "outputs": [
-        {
-          "internalType": "int256",
-          "name": "",
-          "type": "int256"
-        }
-      ],
-      "stateMutability": "pure",
-      "type": "function"
-    }
-  ], '0xB19B8Df2F04A191386F087a64eaa2f7374810304');
-
-  const pr = new Promise((resolve, reject) => {
-    calcContract.methods.getBinaryOperation(firstOperand, secondOperand, operator).call({from: rinkebyWallet}, function(error, result) {
-      if (error) {
-        reject(error);
-      } else {
-        console.log(result);
-        resolve(result);
-      }
-    });
-  });
-
-  return await pr;
-}
+router.post('/calculate', calc.calculate);
 
 module.exports = router;
 
---
+++ src/calc.js
@@ -0,0 +1,64 @@
+const Web3 = require('web3');
+
+const infuraKey = process.env.INFURA_KEY || null;
+const rinkebyWallet = process.env.RINKEBY_WALLET_ADDRESS || null;
+
+const contract = require("../Contracts/calc.json");
+const contract_address = process.env.CONTRACT_ADDRESS || null;
+
+module.exports = {calculate}
+
+async function calculate(req, res) {
+    const first = parseInt(req.body.firstOperand);
+    const second = req.body.secondOperand ? parseInt(req.body.secondOperand): undefined;
+    const operator =  parseInt(req.body.operator);
+
+    try {
+        const result = second? await getBinaryOperation(first, second, operator) :
+            await getUnaryOperation(first, operator);
+        res.send(result.toString());
+    } catch (e) {
+        res.send(e.message)
+    }
+}
+
+async function getUnaryOperation(firstOperand, operator) {
+    const calcContract = getContract();
+
+    const pr = new Promise((resolve, reject) => {
+        calcContract.methods.getUnaryOperation(firstOperand, operator).call({from: rinkebyWallet}, function(error, result) {
+            if (error) {
+                reject(error);
+            } else {
+                console.log(result);
+                resolve(result);
+            }
+        });
+    });
+
+    return await pr;
+}
+
+async function getBinaryOperation(firstOperand, secondOperand, operator) {
+    const calcContract = getContract();
+
+    const pr = new Promise((resolve, reject) => {
+        calcContract.methods.getBinaryOperation(firstOperand, secondOperand, operator).call({from: rinkebyWallet}, function(error, result) {
+            if (error) {
+                reject(error);
+            } else {
+                console.log(result);
+                resolve(result);
+            }
+        });
+    });
+
+    return await pr;
+}
+
+function getContract() {
+    const web3 = new Web3(new Web3.providers.HttpProvider( `https://rinkeby.infura.io/v3/${infuraKey}`));
+    const calcContract = new web3.eth.Contract(contract.output.abi, contract_address);
+
+    return calcContract;
+}
